dc278beef1f001b70f71b8cd4f8bc4b5d51ce134
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-07-22 22:19:48 -0700

    adding connection strings in appsettings and tags to model

---
+++ ProjectWebApp/Data/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebApp.Data
+{
+    public static class Helper
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.Connections
+        }
+    }
+}
