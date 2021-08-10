8f016ae40f21e47afe49041409c765d4f813156d
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-08-10 10:31:52 -0700

    Working mock tests for find all and get by ID

--- ProjectUnitTests/SimulationTests/DataAccess/RepositoryTest.cs
+++ ProjectUnitTests/SimulationTests/DataAccess/RepositoryTest.cs
@@ -18,54 +18,94 @@ namespace ProjectUnitTests.Repositories
 {
     public class RepositoryTest
     {
-        private readonly ISimulationRepository sim_repo; // save the mocked sim repo in a class variable 
-
-        public RepositoryTest()
+        [Fact]
+        public async void SimulationGetAllAsync_RepositoryTest()
         {
-            IList<SimulationModel> simulations = GetSampleSimulations();
+            // Arrange
+            IEnumerable<SimulationModel> simulations = GetSampleSimulations();
 
             // mock the Simulation repository
-            var sim_repo = new Mock<ISimulationRepository>();
-
+            var mock_sim_repo = new Mock<ISimulationRepository>();
+            
             // return all of the simulations 
-            sim_repo.Setup(x => x.GetAllAsync()).ReturnsAsync(simulations.ToList());
+            mock_sim_repo.Setup(x => x.GetAllAsync()).ReturnsAsync(simulations).Verifiable();
 
-            // return a simulation by Id
-            sim_repo.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((int i) => simulations.Where(y => y.Id == i).Single());
+            // mock unit of work 
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            // when unit of work builds a simulation repo, check that it outputs correctly
+            unitOfWorkMock.Setup(x => x.SimulationRepo).Returns(mock_sim_repo.Object);
 
 
-            this.sim_repo = sim_repo.Object;
-        }
+            var sim_repo = mock_sim_repo.Object;
+            var unit_sim_repo = unitOfWorkMock.Object.SimulationRepo; // get a simulation repo from the mocked unitOfWork
 
-        [Fact]
-        public async void ReturnAllSimulations()
-        {
-            // find all products
-            IEnumerable<SimulationModel> test_sims = await this.sim_repo.GetAllAsync();
+            // Act
+            // find all simulations from mocked unitOfWork
+            var actual = await unit_sim_repo.GetAllAsync();
 
+            // find all simulations from mocked Simulation repo
+            IEnumerable<SimulationModel> test_sims = await sim_repo.GetAllAsync();
+
+            // Assert
+            // neither the test or the actual results should be null
             Assert.True(test_sims != null);
+            Assert.True(actual != null);
+
+            // make sure in both that all of the simulations are accounted for
             Assert.Equal(2, test_sims.ToList().Count);
+            Assert.Equal(2, actual.ToList().Count);
+
+            // Compare the lists to make sure that they are equal
+            Assert.Equal(test_sims.ToList(), actual.ToList());
         }
 
         [Fact]
-        public async void ReturnSimulationById()
+        public async void SimulationGetById_RepositoryTest()
         {
-            SimulationModel test_model = await this.sim_repo.GetByIdAsync(1);
+            // Arrange
+            IEnumerable<SimulationModel> simulations = GetSampleSimulations();
 
-            // check that test_model matches what is returned from the repo
-            Assert.True(test_model != null);
-            Assert.Equal(GetSampleSimulations().ToList()[0], test_model);
+            // mock the Simulation repository
+            var mock_sim_repo = new Mock<ISimulationRepository>();
 
-            // test the second model in the repo using the getById method 
-            test_model = await this.sim_repo.GetByIdAsync(2);
+            // By accessing any index within the list of simulations, it the repo should return the  
+            // sim with the corresponding index
+            int any_index = It.IsAny<int>();
+            mock_sim_repo.Setup(x => x.GetByIdAsync(any_index)).ReturnsAsync(simulations.ToList()[any_index]).Verifiable();
 
-            // check that test_model matches what is returned from the repo
-            Assert.True(test_model != null);
-            Assert.Equal(GetSampleSimulations().ToList()[1], test_model);
+            // mock unit of work 
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            // when unit of work builds a simulation repo, check that it outputs correctly
+            unitOfWorkMock.Setup(x => x.SimulationRepo).Returns(mock_sim_repo.Object);
+
+
+            var sim_repo = mock_sim_repo.Object;
+            var unit_sim_repo = unitOfWorkMock.Object.SimulationRepo; // get a simulation repo from the mocked unitOfWork
+
+            // Act
+            // find sim from mocked unitOfWork at any index
+            var actual = await unit_sim_repo.GetByIdAsync(any_index);
+
+            // find product from sim_repo at the same index
+            SimulationModel test_sims = await sim_repo.GetByIdAsync(any_index);
+
+            // Assert
+            // neither the test or the actual results should be null
+            Assert.True(test_sims != null);
+            Assert.True(actual != null);
+
+            // Compare all elements to ensure no data was lost/altered
+            Assert.Equal(test_sims.Id, actual.Id);
+            Assert.Equal(test_sims.simName, actual.simName);
+            Assert.Equal(test_sims.simDesc, actual.simDesc);
+            Assert.Equal(test_sims.gitURL, actual.gitURL);
         }
+
 
         // helper function to generate sample simulations to work with 
-        private IList<SimulationModel> GetSampleSimulations()
+        private IEnumerable<SimulationModel> GetSampleSimulations()
         {
             List<SimulationModel> models = new List<SimulationModel>
             {
