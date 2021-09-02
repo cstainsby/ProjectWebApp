072d26c816d97ce50a6c794080245c3ca5a31182
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-09-02 10:09:12 -0700

    testing push through linux subsystem, adding UML for c++ simulations

--- ProjectDLLs/TwoDimSimulation_FluidWrapper.h
+++ ProjectDLLs/TwoDimSimulation_FluidWrapper.h
@@ -6,22 +6,6 @@
 class TwoDimSimulation_FluidWrapper : public TwoDimSimulationTypeWrapper
 {
 public:
-	// pointer to current fluid simulation project
-	TwoDimSimulation_Fluid* project;
-
-	// function lists for settings and views
-	// kv list, key: string identifier, value: setting setter functions relating to string
-	typedef void (TwoDimSimulation_Fluid::* settingSetterFunc)(float);
-	ArrayListCollection<std::string, settingSetterFunc> settingInteractionList;
-
-	// kv list, key: string identifier, value: view getter functions relating to string
-	typedef void (TwoDimSimulation_Fluid::* viewReturnFunc)(float*&);
-	ArrayListCollection<std::string, viewReturnFunc> viewInteractionList;
-	
-	// kv list, key: string identifier, value: view getter functions relating to string
-	typedef void(TwoDimSimulation_Fluid::* userInteractionFunc)(int xCord, int yCord);
-	ArrayListCollection<std::string, userInteractionFunc> userInteractionList;
-
 	// use dependancy injection to link a given project with dll calls
 	TwoDimSimulation_FluidWrapper(TwoDimSimulation_Fluid* project);
 
@@ -39,4 +23,21 @@ public:
 
 	// update to next step
 	void update() const;
+
+private:
+	// pointer to current fluid simulation project
+	TwoDimSimulation_Fluid* project;
+
+	// function lists for settings and views
+	// kv list, key: string identifier, value: setting setter functions relating to string
+	typedef void (TwoDimSimulation_Fluid::* settingSetterFunc)(float);
+	ArrayListCollection<std::string, settingSetterFunc> settingInteractionList;
+
+	// kv list, key: string identifier, value: view getter functions relating to string
+	typedef void (TwoDimSimulation_Fluid::* viewReturnFunc)(float*&);
+	ArrayListCollection<std::string, viewReturnFunc> viewInteractionList;
+
+	// kv list, key: string identifier, value: view getter functions relating to string
+	typedef void(TwoDimSimulation_Fluid::* userInteractionFunc)(int xCord, int yCord);
+	ArrayListCollection<std::string, userInteractionFunc> userInteractionList;
 };
