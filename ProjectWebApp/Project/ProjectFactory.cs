60b34b7ba2dd5ddee45114481a1f1bda61c17786
cstainsby <cstainsby@zagmail.gonzaga.edu>
2021-09-12 00:30:44 -0700

    making c# version of implementation to simplify process, less dll fun

---
+++ ProjectWebApp/Project/ProjectFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebApp.Project
+{
+    public class ProjectFactory
+    {
+        /*
+         * should be able to call and retrieve a project object with the specific implementation based on parameter package
+         */
+        public ProjectWrapper createProject(String projectType, String implementationName)
+        {
+            if (projectType == "TwoDimSim")
+            {
+
+            }
+        }
+    }
+}
